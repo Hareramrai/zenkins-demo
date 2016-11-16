2440807ebf467c3cb044de19515ab629b4177b6d
Hare Ram Rai <guru.java.ram@gmail.com>
2016-11-17 01:04:13 +0530

    Add docker settings

--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,20 @@
 
 ActiveRecord::Schema.define(version: 20161106140639) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- lib/tasks/ci.rake
+++ lib/tasks/ci.rake
@@ -1,5 +1,4 @@
-require 'ci/reporter/rake/rspec'
-task :rspec => 'ci:setup:rspec'
-# namespace :ci do
-#   task :all => ['ci:setup:rspec', 'rspec']
-# end
+unless Rails.env.production?
+  require 'ci/reporter/rake/rspec'
+  task :rspec => 'ci:setup:rspec'
+end
